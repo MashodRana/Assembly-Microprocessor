26654041b5768eb7702105e8df93497c82407e06
Mashod Rana <38730871+MashodRana@users.noreply.github.com>
2018-08-05 03:29:59 +0600

    Create Dec2Bin.asm

---
+++ Assembly Code/Dec2Bin.asm
@@ -0,0 +1,77 @@
+; *******************Decimal to Binary in Assembly***********
+;   *********author: Slack_Coder(mashod0rana@gmail.com) ***"know yourself"
+;       *********University of Asia Pacific
+;--------------------------------------------------------------------------------------------------------
+
+INCLUDE EMU8086.INC
+.MODEL SMALL
+.STACK 100H
+.DATA
+    INPUT DB 0
+    X DB 10
+    ARRAY db 100 DUP(?)
+    DIVIDER DB 2 
+
+.CODE
+ 
+    MAIN PROC
+ 
+        MOV AX,@DATA        ;FOR DATA SEGMENT
+        MOV DS,AX
+
+;----------As a Input Function--------- 
+print "Enter a Decimal number Less than 256: "   
+INPUT_LABEL:
+MOV AH,1
+    INT 21H
+    CMP AL,13
+    JE print_comment
+    SUB AL,48
+    MOV CL,AL
+    MOV AL,INPUT
+    MUL X
+    ADD AL,CL
+    MOV INPUT,AL  
+    JMP INPUT_LABEL
+                   
+                   
+;------for Divide by 2 to get BinaryNumber
+print_comment:
+    printn  
+    printn
+    print "Answer in Binary is: "
+                   
+DIVIDE:
+    MOV AH,0
+    MOV AL,INPUT
+    DIV DIVIDER
+    MOV ARRAY[DI],AH
+    INC DI
+    CMP AL,0
+    JE RESULT
+    MOV INPUT,AL
+    JMP DIVIDE
+    
+
+            
+RESULT:
+    DEC DI
+    CMP DI,0
+    JL EXIT
+    MOV DL,ARRAY[DI]
+    ADD DL,48
+     
+    MOV AH,2
+    INT 21H
+    JMP RESULT
+
+    
+exit:
+    mov ah,4ch
+    int 21h
+    
+MAIN ENDP
+    END MAIN
+   
+
+    
